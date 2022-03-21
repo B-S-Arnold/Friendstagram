8427d5dc43a8c3a2612a8c5b75d148a80bfdf3b8
Bryan <bryanscottarnold@gmail.com>
2022-03-21 15:21:31 -0400

    added dropdown

---
+++ react-app/src/components/Dropdown.js
@@ -0,0 +1,50 @@
+import { useDispatch, useSelector } from "react-redux"
+import { NavLink, useHistory } from "react-router-dom"
+import { logout } from "../store/session";
+
+const Dropdown = () => {
+
+    const user = useSelector(state => state.session.user)
+    const history = useHistory()
+
+    const LogoutButton = () => {
+        const dispatch = useDispatch()
+        const onLogout = async (e) => {
+            await dispatch(logout());
+        };
+
+        return <div className='dropdownbtn logoutbtn' onClick={onLogout}>Logout</div>;
+    };
+
+    const ProfileButton = () => {
+
+        const toProfile = () => {
+            history.push(`/${user.username}`);
+        }
+
+        return <div className='dropdownbtn profbtn' onClick={toProfile}>Profile</div>
+        // return <NavLink to={`/${user.username}`} exact={true} activeClassName='active'>
+        //     Profile
+        // </NavLink>
+    };
+
+
+    return (
+        <>
+        <div className="dropdown">
+            
+            <div>
+                <ProfileButton />
+
+                {/* <NavLink className='dropdownbtn' to={`/${user.username}`} exact={true} activeClassName='active'>
+                    Profile
+                </NavLink> */}
+            </div>
+            <div>
+                <LogoutButton  />
+            </div>
+        </div>
+                </>
+    )}
+
+export default Dropdown;
