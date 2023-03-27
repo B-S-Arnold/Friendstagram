70deed0db8a394d5a5b15d9016ee149b94761f82
Bryan <bryanscottarnold@gmail.com>
2023-03-27 14:56:31 -0400

    fixed seeds

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -8,11 +8,11 @@ def seed_users():
     tom = User(
         username='iLoveMyWife25', email='tomwambsgams@aa.io', password='password', fullName='Tom Wambsgams', url='https://media.gq.com/photos/5da61bb0b2ccb20008b494be/4:3/w_2551,h_1913,c_limit/tv-succession-matthew-macfadyen-tom-wambsgans-gq-october-101519.jpg')
     greg = User(
-        username='theOneGregHirsch', email='greghirsch@aa.io', password='password', fullName='Greg Hirsch', url='https://static.wikia.nocookie.net/succession/images/0/02/Sporuslol.png/revision/latest?cb=20220425193919')
+        username='theOneGregHirsch', email='greghirsch@aa.io', password='password', fullName='Greg Hirsch', url='https://media-cldnry.s-nbcnews.com/image/upload/t_fit-760w,f_auto,q_auto:best/newscms/2021_41/1788465/cousin-greg-kb-inline-5-211015.jpg')
     peggy = User(
         username='peggyhill54', email='margarethill@aa.io', password='password', fullName='Margaret Hill', url='https://www.giantbomb.com/a/uploads/scale_small/0/7383/1813631-peggy_hill.jpg')
     bart = User(
-        username='elbarto', email='bartman@aa.io', password='password', fullName='El Barto', url='https://w0.peakpx.com/wallpaper/644/931/HD-wallpaper-bart-simpson.jpg')
+        username='elbarto', email='bartman@aa.io', password='password', fullName='El Barto', url='https://e1.pxfuel.com/desktop-wallpaper/591/300/desktop-wallpaper-bartsimpson-el-barto.jpg')
     carl = User(
         username='2cool4U', email='carl@aa.io', password='password', fullName='Carl Brutananadilewski', url='https://i.cdn.turner.com/asfix/repository//8a25c39212c8bcf30112c99af0910001/thumbnail_63404.jpg')
     butters = User(
