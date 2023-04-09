a3945e2cde2a27258e403fc90a74397290c303d4
Bryan <bryanscottarnold@gmail.com>
2023-04-08 23:26:45 -0400

    follow seeds

---
+++ app/seeds/follows.py
@@ -0,0 +1,22 @@
+from app.models import db, Follow
+
+
+def seed_follows():
+    
+   
+    
+    follow_seeds = []
+    
+    for i in range(2, 24):
+        for j in range(1, 24):
+            if i != j:
+                follow_seeds.append(Follow(userId=str(i), followedId=str(j)))
+                
+    db.session.add_all(follow_seeds)
+   
+    db.session.commit()
+
+
+def undo_follows():
+    db.session.execute('TRUNCATE follows RESTART IDENTITY CASCADE;')
+    db.session.commit()
